3dd1c8e239e1365523c44878e61ba625250eb63c
Priyanshu Kumar Raj <95175056+priyanshu2552@users.noreply.github.com>
2024-07-12 02:08:35 +0530

    Delete server directory

--- server/routes/UsersRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { Signup, Login } = require("../controllers/UsersControllers");
-router.post("/signup", Signup);
-router.post("/login", Login);
-module.exports = router;
